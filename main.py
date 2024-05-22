36ef9f33e3faac4cb75938e8cb7183a80cd69c3b
pvtx4ditya <166001017+pvtx4ditya@users.noreply.github.com>
2024-05-22 17:49:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,37 +6,50 @@ from keep_alive import keep_alive
 
 keep_alive()
 
-def download_file(url, name="video.mp4"):
-  os.system(f"wget {url}")
-  temp_name = url.split("/")[-1]
-  os.system(f"cp {temp_name} {name}")
-  os.remove(temp_name)
+def download_file(url):
+    os.system(f"wget {url}")
+    temp_name = url.split("/")[-1]
+    os.system(f"cp {temp_name} {temp_name}.mp4")
+    os.remove(temp_name)
 
 
-TOKEN = os.environ['Token']
+TOKEN = os.environ["Token"]
 bot = telebot.TeleBot(TOKEN)
 
 
 @bot.message_handler(commands=["start"])
 def start(message):
-  bot.send_message(message.chat.id, "Send Link in format /link [url]")
+    bot.send_message(message.chat.id, "Send Link in format /link [url]")
 
 
 @bot.message_handler(commands=["link"])
 def link(message):
-  link = message.text.split(" ")[-1].split("/")[-1].replace(" ", "")
-  resp = download_terabox_video("https://terabox.hnn.workers.dev", link,
-                                None).replace(" ", "")
-  bot.send_message(message.chat.id, resp)
-  print(resp)
-  download_file(resp, name="video.mp4")
-  time.sleep(2)
-  vid = open("video.mp4", "rb")
-  bot.send_video(message.chat.id, vid)
+    link = message.text.split("https://")[-1].split("/s/")[-1]
+    print(link)
+    resp = download_terabox_video("https://terabox.hnn.workers.dev", link, None).replace(" ", "")
+    bot.send_message(message.chat.id, resp)
+    download_file(resp)
+    time.sleep(2)
+    with open(f"{resp.split("/")[-1]}.mp4", "rb") as vid:
+      bot.send_video(message.chat.id, vid)
+    os.remove(f"{resp.split("/")[-1]}.mp4")
+
+
+@bot.message_handler(content_types=['text'])
+def check_forwarded(message):
+    link = message.text.split("https://")[-1].split("/s/")[-1]
+    print(link)
+    resp = download_terabox_video("https://terabox.hnn.workers.dev", link, None).replace(" ", "")
+    bot.send_message(message.chat.id, resp)
+    download_file(resp)
+    time.sleep(2)
+    with open(f"{resp.split("/")[-1]}.mp4", "rb") as vid:
+      bot.send_video(message.chat.id, vid)
+    os.remove(f"{resp.split("/")[-1]}.mp4")
 
 
 while True:
-  try:
-    bot.polling()
-  except:
-    time.sleep(5)
+    try:
+        bot.polling()
+    except:
+        time.sleep(5)
